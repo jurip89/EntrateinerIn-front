05808d12e333c1a142dfcebc4f3be0380a7aacc1
jurip89 <juri@Barbaras-iMac.local>
2022-11-11 16:57:54 +0100

    map problems maybe solved

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 export const URL = 'https://entrateinerin-back-production.up.railway.app'
 export const MAP_BOX_KEY =
-  process.env.REACT_APP_MAP_BOX_KEY || 'pk.eyJ1IjoianVyaXA4OSIsImEiOiJjbDh5ZXdja3AwN3hnM25wazVxNGZpNTY5In0.icMSIxcMKGKhrZ3D7gT2QA';
+  process.env.REACT_APP_MAP_BOX_KEY || 'pk.eyJ1IjoianVyaXA4OSIsImEiOiJjbGFjb21jamwwY2UwM3BydHJiOWpqdW4xIn0.0M_uGjWX3WakTXhFzjmlBw';
 export const CLOUD_KEY = process.env.REACT_APP_CLOUD_KEY || 'dmk9xo60f';
