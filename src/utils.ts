0293f878e4c05bbf348fff4975dffd04ea67a567
jurip89 <juri@Barbaras-iMac.local>
2022-11-11 16:26:57 +0100

    maybe it works in the deploy now

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const URL = process.env.REACT_APP_URL ||"http://localhost:4000";
+export const URL = process.env.REACT_APP_URL 
 export const MAP_BOX_KEY =
   process.env.REACT_APP_MAP_BOX_KEY;
 export const CLOUD_KEY = process.env.REACT_APP_CLOUD_KEY;
